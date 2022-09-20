98fd2fea781439fcac91b14fa7e2ad508cf90b11
JMaylor <60825814+JMaylor@users.noreply.github.com>
2022-09-20 21:48:28 +0100

    docs favicon

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,6 +1,15 @@
 export default {
   title: 'Maylor',
   description: 'Football Data.',
+  head: [
+    ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' }],
+    ['link', { rel: 'icon', sizes: '32x32', href: '/favicon-32x32.png' }],
+    ['link', { rel: 'icon', sizes: '16x16', href: '/favicon-16x16.png' }],
+    ['link', { rel: 'manifest', href: '/site.webmanifest' }],
+    ['link', { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#b91d47' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#b91d47' }],
+    ['meta', { name: 'theme-color', content: '#ffffff' }],
+  ],
   themeConfig: {
     logo: '/logo.png',
 
