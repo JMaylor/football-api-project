92e58671d7f0070aed7d5e4706371bfb62c40c28
JMaylor <60825814+JMaylor@users.noreply.github.com>
2022-10-13 12:57:22 +0100

    init fastify

---
+++ api/app.js
@@ -0,0 +1,33 @@
+import Fastify from 'fastify'
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+import autoLoad from '@fastify/autoload'
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
+dotenv.config()
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+/**
+ * @type {import('fastify').FastifyInstance} Instance of Fastify
+ */
+const fastify = Fastify({
+  logger: true
+})
+
+// autoload plugins
+fastify.register(autoLoad, {
+  dir: join(__dirname, 'plugins')
+})
+
+// autoload routes
+fastify.register(autoLoad, {
+  dir: join(__dirname, 'routes')
+})
+
+fastify.listen({ port: 5000 }, function (err, address) {
+  if (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+})
