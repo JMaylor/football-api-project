328f5976445e0ef24c1adffc4ea409fdc3596c4a
Joe Maylor <60825814+JMaylor@users.noreply.github.com>
2022-10-14 14:06:32 +0100

    Update postgres.js

--- api/plugins/postgres.js
+++ api/plugins/postgres.js
@@ -3,5 +3,6 @@ import fastifyPlugin from 'fastify-plugin'
 export default fastifyPlugin(async (fastify, options) => {
   fastify.register(import('@fastify/postgres'), {
     connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
   })
 })
