164ad4121612b62cac2dee6064f18b84166ecbab
JMaylor <60825814+JMaylor@users.noreply.github.com>
2022-10-15 18:17:17 +0100

    simplify pg connection and configure development vs production

--- api/plugins/postgres.js
+++ api/plugins/postgres.js
@@ -2,13 +2,9 @@ import fastifyPlugin from 'fastify-plugin'
 
 export default fastifyPlugin(async (fastify, options) => {
   fastify.register(import('@fastify/postgres'), {
-    user: process.env.USERNAME,
-    host: process.env.HOSTNAME,
-    database: process.env.DATABASE,
-    password: process.env.PASSWORD,
-    port: process.env.DB_PORT,
     ssl: {
-      ca: process.env.CA_CERT
-    },
+      require: true,
+      rejectUnauthorized: process.env.NODE_ENV !== 'development'
+    }
   })
 })
