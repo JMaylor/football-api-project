9219d398a256b94d5d8972ed03bfb7bfe2932de2
JMaylor <joseph.maylor@gmail.com>
2023-12-26 12:44:17 +0000

    feat: init nuxt frontend

---
+++ client/components/TeamList.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+const supabase = useTypedSupabaseClient()
+
+const {data: teams} = await supabase.from('team').select('*').order('team_name', { ascending: true })
+const team = ref<string>()
+</script>
+
+<template>
+  <USelectMenu v-model="team" searchable :options="teams" option-attribute="team_name" value-attribute="team_name" class="w-40" v-if="teams" />
+  {{ team }}
+</template>
